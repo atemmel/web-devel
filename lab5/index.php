3c88438cc23ddb22c6f2a5eaa390c4b77adabf51
atemmel <temmeladam@gmail.com>
2019-02-19 09:16:53 +0100

    added something

---
+++ lab5/index.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+	<div id="main">
+		<div id="navigation">
+			<!--
+			<div class="navigation-button">Home</div>
+			<div class="navigation-button">About</div>
+			<div class="navigation-button">Projects</div>
+			<div class="navigation-button">Contact</div>
+			-->
+
+			<a href="#home">Home</a>
+			<a href="#home">About</a>
+			<a href="#home">Projects</a>
+			<a href="#home">Contact</a>
+		</div>
+	</div>
+</body>
+</html>
