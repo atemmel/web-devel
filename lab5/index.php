28d76cc2d213cc4d72aeac2b0a0ba7f3ec30e3f0
atemmel <temmeladam@gmail.com>
2019-03-03 07:18:41 +0100

    Delvis fungerande för mobiler

--- lab5/index.php
+++ lab5/index.php
@@ -3,57 +3,58 @@
 ?>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="stylesheet" type="text/css" href="index.css">
 	<link href="https://fonts.googleapis.com/css?family=B612" rel="stylesheet"> 
 	<script src="main.js"></script>
 </head>
 <body>
 	<div id="document">
-	<div id="navigation-sticky">
-		<a href="#contact">Contact</a>
-		<a href="#projects">Projects</a>
-		<a href="#about">About</a>
-		<a href="#home">Home</a>
-	</div>
-	<div id="navigation">
-		<a href="#contact">Contact</a>
-		<a href="#projects">Projects</a>
-		<a href="#about">About</a>
-		<a href="#home">Home</a>
-	</div>
-	<div id="banner">
-		<div class="circle-icon-container">
+		<div id="navigation-sticky">
+			<a href="#contact">Contact</a>
+			<a href="#projects">Projects</a>
+			<a href="#about">About</a>
+			<a href="#home">Home</a>
 		</div>
-		<div class="text-block">
-			<h1>Lorem Ipsum</h1>
-			Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+		<div id="navigation">
+			<a href="#contact">Contact</a>
+			<a href="#projects">Projects</a>
+			<a href="#about">About</a>
+			<a href="#home">Home</a>
 		</div>
-	</div>
-	<div id="main">
-		<div class="vspace">
-			<div class="section" id="about">
-				<h1>About</h1>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		<div id="banner">
+			<div class="circle-icon-container">
+			</div>
+			<div class="text-block">
+				<h1>Lorem Ipsum</h1>
+				Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
 			</div>
 		</div>
-		<div class="vspace">
-			<div class="section" id="projects">
-				<div class="project">1</div>
-				<div class="project">2</div>
-				<div class="project">3</div>
-				<div class="project">4</div>
-				<div class="project">5</div>
-				<div class="project">6</div>
-				<div class="project">7</div>
+		<div id="main">
+			<div class="vspace">
+				<div class="section" id="about">
+					<h1>About</h1>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</div>
+			</div>
+			<div class="vspace">
+				<div class="section" id="projects">
+					<div class="project">1</div>
+					<div class="project">2</div>
+					<div class="project">3</div>
+					<div class="project">4</div>
+					<div class="project">5</div>
+					<div class="project">6</div>
+					<div class="project">7</div>
+				</div>
+			</div>
+			<div class="vspace">
+				<div class="section" id="contact">
+					<h1>Contact</h1>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</div>
 			</div>
 		</div>
-		<div class="vspace">
-			<div class="section" id="contact">
-				<h1>Contact</h1>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-			</div>
-		</div>
-	</div>
 	</div>
 </body>
 </html>
